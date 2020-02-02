5341d05a14f792350db86230bb8143f3356496aa
Ryuu11 <60558023+Ryuu11@users.noreply.github.com>
2020-02-02 17:31:47 -0400

    Ejercicio 2

---
+++ Prog3Tarea2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Prog3Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] z = new int[10];
+            int num;
+            for (int i = 0; i <z.Length; i++)
+            {
+                Console.Write("Escribe tus 10 numeros: ");
+                num = int.Parse(Console.ReadLine());
+                z[i] = num;
+
+            }
+
+            Console.WriteLine("El numero mayor es: " + z.Max());
+            Console.WriteLine("El numero mayor es: " + z.Min());
+        }
+    }
+}
